9abc9faed5b4d88f859b265f41f3e540add163cc
mariamshamy <20812022200580@eng.zu.edu.eg>
2025-10-12 23:08:02 +0300

    Add files via upload

---
+++ labs/lab1/taskWork1.asm
@@ -0,0 +1,22 @@
+.data
+name: .asciiz "name: mariam elshamy   \n"
+id: .asciiz "ID: 20812022200580    \n"
+course: .asciiz "Course: CSE321-computer-organization  \n"
+
+.text
+
+main:
+li $v0,4
+la $a0,name
+syscall
+
+li $v0,4
+la $a0,id
+syscall
+
+li $v0,4
+la $a0,course
+syscall
+
+li $v0,10
+syscall
